c4df97ea9cd857572210f063a1070f0da3fddb53
dor1633 <dor1633@gmail.com>
2023-02-03 17:41:15 +0200

    move to library

--- lib/cache/cache.module.ts
+++ lib/cache/cache.module.ts
@@ -1,36 +1,16 @@
-/* eslint-disable prettier/prettier */
 import {
-    CacheModuleAsyncOptions,
-    CacheModuleOptions,
     DynamicModule,
     Module,
 } from '@nestjs/common';
 import { ConfigurableModuleClass } from '@nestjs/common/cache/cache.module-definition';
 import { createProviders } from './cache.provider';
 
-
-
-
 @Module({})
 export class CacheModule extends ConfigurableModuleClass {
     static register(options: Record<string, any>[]): DynamicModule {
         const providers = createProviders(options);
         return {
-            // global: options.isGlobal,
             ...super.register(options),
-            providers,
-            exports: providers
-        };
-    }
-
-    static registerAsync(
-        options: Record<string, any>[],
-    ): DynamicModule {
-        const moduleDefinition = super.registerAsync(options);
-        const providers = createProviders(options);
-
-        return {
-            ...moduleDefinition,
             providers,
             exports: providers
         };
