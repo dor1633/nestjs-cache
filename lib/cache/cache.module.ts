089760e2ecd0e9aca1dde3cf4cdc71c9602ce06f
dor1633 <dor1633@gmail.com>
2023-02-04 15:39:57 +0200

    add cache-metadata type

--- lib/cache/cache.module.ts
+++ lib/cache/cache.module.ts
@@ -5,11 +5,12 @@ import {
 } from '@nestjs/common';
 import { ConfigurableModuleClass } from '@nestjs/common/cache/cache.module-definition';
 import { AsyncProviderFactory } from 'lib/types/async-provider-factory';
+import { CacheMetadata } from 'lib/types/cache-metadata';
 import { createAsyncProviders, createProviders } from './cache.providers';
 
 @Module({})
 export class CacheModule extends ConfigurableModuleClass {
-    static register(options: Record<string, any>[]): DynamicModule {
+    static register(options: CacheMetadata[]): DynamicModule {
         const providers = createProviders(options);
         return {
             ...super.register(options),
--- lib/cache/cache.providers.ts
+++ lib/cache/cache.providers.ts
@@ -1,8 +1,9 @@
 import { Provider } from '@nestjs/common';
 import { caching } from 'cache-manager'
 import { AsyncProviderFactory } from 'lib/types/async-provider-factory';
+import { CacheMetadata } from 'lib/types/cache-metadata';
 
-export function createProviders(configs: Record<string, any>[]): Provider[] {
+export function createProviders(configs: CacheMetadata[]): Provider[] {
     return configs.map((config) => {
         return {
             provide: config.cacheName,
